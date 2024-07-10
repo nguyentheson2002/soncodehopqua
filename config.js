de2e3464991003554370573c2603fd192c6f4fd9
nguyentheson2002 <nguyentheson.yt13062002@gmail.com>
2024-07-10 16:50:05 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Lâm Yến Nhi';
+const giftUrl = 'https://www.facebook.com/nhi.lamyen.739';
+const eventName = 'Anh Yêu Em';
+const titleCard = 'Tặng công chúa của anh';
+const contentCard = 'Chúc vợ yêu của anh thật nhiều sức khoẻ và những nụ cười. Mong những điều tốt đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau nhé.Anh yêu Em     ';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
